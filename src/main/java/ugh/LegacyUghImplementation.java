b2d933116ced462893ecc9c21f5540131b8c8112
Matthias Ronge <matthias.ronge@zeutschel.de>
2018-02-09 08:32:22 +0100

    Add a license header to LegacyUghImplementation

--- src/main/java/ugh/LegacyUghImplementation.java
+++ src/main/java/ugh/LegacyUghImplementation.java
@@ -1,3 +1,14 @@
+/*
+ * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+ *
+ * This file is part of the Kitodo project.
+ *
+ * It is licensed under GNU General Public License version 3 or later.
+ *
+ * For the full copyright and license information, please read the
+ * GPL3-License.txt file that was distributed with this source code.
+ */
+
 package ugh;
 
 import org.kitodo.api.ugh.ContentFileInterface;
@@ -17,6 +28,7 @@ import org.kitodo.api.ugh.RomanNumeralInterface;
 import org.kitodo.api.ugh.VirtualFileGroupInterface;
 import org.kitodo.api.ugh.exceptions.MetadataTypeNotAllowedException;
 import org.kitodo.api.ugh.exceptions.PreferencesException;
+
 import ugh.dl.ContentFile;
 import ugh.dl.DigitalDocument;
 import ugh.dl.Metadata;
