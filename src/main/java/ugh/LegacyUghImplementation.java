c3c0491e956a8e25ee839ef5d602788258fa67b5
Matthias Ronge <matthias.ronge@zeutschel.de>
2018-02-16 09:33:57 +0100

    Use UGH license header

--- src/main/java/ugh/LegacyUghImplementation.java
+++ src/main/java/ugh/LegacyUghImplementation.java
@@ -1,13 +1,23 @@
-/*
- * (c) Kitodo. Key to digital objects e. V. <contact@kitodo.org>
+/*******************************************************************************
+ * ugh / LegacyUghImplementation.java
  *
- * This file is part of the Kitodo project.
+ * Copyright 2010 Center for Retrospective Digitization, Göttingen (GDZ)
  *
- * It is licensed under GNU General Public License version 3 or later.
+ * http://gdz.sub.uni-goettingen.de
  *
- * For the full copyright and license information, please read the
- * GPL3-License.txt file that was distributed with this source code.
- */
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This Library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 
 package ugh;
 
